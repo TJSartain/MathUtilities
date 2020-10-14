b3c5e34d891e771f6f39823d01cf0261a077c564
TJ Sartain <tj@iTrinity-Inc.com>
2020-10-13 23:20:52 -0400

    added Int extensions

--- Sources/MathUtilities/MathExtensions.swift
+++ Sources/MathUtilities/MathExtensions.swift
@@ -5,6 +5,33 @@
 //  Created by TJ Sartain on 10/13/20.
 //
 
+import UIKit
+
+extension Int
+{
+    func pin(_ a: Int, _ b: Int) -> Int
+    {
+        let m = (a > self ? a : self)
+        return (m < b ? m : b)
+    }
+    
+    func factorial() -> Int
+    {
+        if self == 1 || self == 0 {
+            return 1
+        } else if self < 0 {
+            return -1
+        }
+        return self * (self-1).factorial()
+    }
+    
+    var radians: CGFloat
+    {
+        CGFloat(self).radians
+    }
+    
+}
+
 
 extension Double
 {
